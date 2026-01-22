4dec8ebdf4d478f12a752d87258c0fc757d10d96
Ashraf Wajdi <ashrfwjdii@gmail.com>
2026-01-22 13:16:37 +0800

    Initial commit - project setup

---
+++ wheeldeal-backend/src/main/java/com/wheeldeal_backend/WheeldealBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wheeldeal_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheeldealBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheeldealBackendApplication.class, args);
+	}
+
+}
---
+++ wheeldeal-backend/src/test/java/com/wheeldeal_backend/WheeldealBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wheeldeal_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheeldealBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
